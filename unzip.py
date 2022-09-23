1a38d0acd51cbea2e880d2316862f4e40d1604fb
Tong Shen <tong.shen@convatec.com>
2022-09-23 17:18:32 -0400

    added rar fixed filename bug

--- unzip.py
+++ unzip.py
@@ -1,32 +1,45 @@
 from zipfile import ZipFile
 import os
 from termcolor import colored
+import patoolib
 
 count = 0
 
 
 def walk(startroot):
-    print('Current directory: ', startroot)
     for root, dirs, files in os.walk(startroot):
         for filenames in files:
-            if filenames.endswith('.zip'):
+            if filenames.endswith('.zip') or filenames.endswith('.Zip'):
                 print('zipfile {} found in: {}'.format(filenames, root))
                 currentdir = unziponce(root, filenames)
                 walk(currentdir)
-
+            elif filenames.endswith('rar'):
+                currentdir = unziponce(root, filenames)
+                walk(currentdir)
 
 def unziponce(currentdir, zipfilename):
     global count
-    with ZipFile(currentdir + '/' + zipfilename) as zipObject:
-        copytofoldername = zipfilename.split('.')[-2].rstrip()
+    copytofoldername = zipfilename[:-4].rstrip()
+    if zipfilename.endswith('.rar'):
         if copytofoldername in os.listdir(currentdir):
-            print(colored('a folder with same name already exist take note: ' + currentdir +"/"+ zipfilename, 'red'))
+            copytofoldername+='torar'
+            print(colored('a folder with same name already exist take note: ' + currentdir + "/" + zipfilename, 'red'))
+        os.mkdir(currentdir + '/' + copytofoldername)
+        patoolib.extract_archive(currentdir + '/' + zipfilename, outdir=currentdir + '/' + copytofoldername)
+        print(colored('unzipped: {} in {}\n'.format(zipfilename, currentdir), 'green'))
+        count += 1
+        return currentdir + "/" + copytofoldername
+
+    with ZipFile(currentdir + '/' + zipfilename) as zipObject:
+        if copytofoldername in os.listdir(currentdir):
+            print(colored('a folder with same name already exist take note: ' + currentdir + "/" + zipfilename, 'red'))
             with open('note.txt', 'a') as f:
                 f.write(currentdir + " " + zipfilename + "\n")
             copytofoldername += 'fromzip'
-        print('unziping: {} in {}'.format(zipfilename, currentdir))
-        zipObject.extractall(currentdir + '/' + copytofoldername)
-        print('unzipped: {} in {}'.format(zipfilename, currentdir))
+        if zipfilename.endswith('.zip') or zipfilename.endswith('.Zip'):
+            print('unziping: {} in {}'.format(zipfilename, currentdir))
+            zipObject.extractall(currentdir + '/' + copytofoldername)
+            print(colored('unzipped: {} in {}\n'.format(zipfilename, currentdir),'green'))
         count += 1
         return currentdir + "/" + copytofoldername
 
